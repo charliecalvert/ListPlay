7e2489aaab77872d5d5d3c880f4f925493eb21c7
Charles Calvert <java@elvenware.com>
2013-01-17 11:09:21 -0800

    Initial Commit

---
+++ ListPlay.js
@@ -0,0 +1,29 @@
+/**
+ * @author Charlie Calvert
+ */
+
+var MyObject = (function() {
+	'use strict';
+	
+	function MyObject() {
+		
+	}
+	
+	MyObject.prototype.clear = function() {
+		$("#myList").empty();
+	}
+	
+	MyObject.prototype.addItems = function() {
+		$("#myList").append("<li>New Item01</li>");
+		$("#myList").append("<li>New Item02</li>");
+		$("#myList").append("<li>New Item03</li>");
+	}
+	
+	return MyObject;
+})();
+
+$(document).ready(function() {
+	var myObject = new MyObject();
+	$("#buttonClear").click(myObject.clear);
+	$("#buttonAdd").click(myObject.addItems);
+});
